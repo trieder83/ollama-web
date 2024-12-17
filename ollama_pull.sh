7735770459e37b2f4eaf97c55d0e5612e0007a61
I <trieder@charon>
2024-12-17 22:06:23 +0100

    add info for multilingual embeddings

--- ollama_pull.sh
+++ ollama_pull.sh
@@ -2,6 +2,11 @@
 
 # kubectl exec -i ${pod}  -n default --  ollama create comint -f /root/model/Modelfile
 
+# multilingual embeddings
+# * https://ollama.com/library/bge-m3
+# * https://ollama.com/library/snowflake-arctic-embed2
+# * https://ollama.com/library/paraphrase-multilingual
+
 # First, find the pod name
 kubectl get pods -l app=ollama -n default
 pod=$(kubectl get pods -n default -l app=ollama -o=jsonpath='{.items[0].metadata.name}')
